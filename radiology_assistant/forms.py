2bb2f0be18902ebf4134ae6e0dc7ffea5e152563
McDareth <McDareth@gmail.com>
2020-11-20 22:32:28 -0500

    Added import and UpdateAccountForm class to forms.py

--- radiology_assistant/forms.py
+++ radiology_assistant/forms.py
@@ -4,6 +4,7 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextA
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from radiology_assistant.models import User
 from flask import request
+from flask_login import current_user
 class RegistrationForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired(), Length(min=1, max=50)])
     lastname = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=50)])
@@ -47,3 +48,24 @@ class SearchForm(FlaskForm):
         if 'csrf_enabled' not in kwargs:
             kwargs['csrf_enabled'] = False
         super().__init__(*args, **kwargs)
+
+class UpdateAccountForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired(), Length(min=1, max=50)])
+    lastname = StringField('Last Name', validators=[DataRequired(), Length(min=1, max=50)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[])
+    confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password')])
+    submit = SubmitField('Update Account')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
